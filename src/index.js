7a9860c087e478a7408764382a46dc2ed32a26e5
Gustavo Lidani <lidanig0@gmail.com>
2022-04-15 19:57:11 -0300

    router and firebase initialization

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./components/App/App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { initializeApp } from "firebase/app";
+
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+
+const firebaseConfig = require("./firebase.config.json");
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route exact path="*" element={<App firebase={app} />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
