c8c95773b901f984c60b5ccca94acd9cec66ae92
Gustavo Lidani <lidanig0@gmail.com>
2022-04-17 00:01:30 -0300

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "react-app",
+    "react-app/jest",
+    "plugin:security-node/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "security-node"],
+  rules: {
+    // desabilitar pois não estou usando TS
+    "react/prop-types": "off",
+  },
+};
